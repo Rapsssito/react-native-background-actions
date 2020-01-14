b80b56d7a2a29bdd932fd74f391a68a4033e05bd
Rapsssito <contact@rodrigomartin.dev>
2020-01-14 22:21:33 +0100

    fix: Android resolve promises (#3)

--- android/src/main/java/com/asterinet/react/bgactions/BackgroundActionsModule.java
+++ android/src/main/java/com/asterinet/react/bgactions/BackgroundActionsModule.java
@@ -6,28 +6,35 @@ import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+import androidx.annotation.NonNull;
+
 @SuppressWarnings("WeakerAccess")
 public class BackgroundActionsModule extends ReactContextBaseJavaModule {
 
     private final ReactContext reactContext;
+
+    private final String TAG = "RNBackgroundActions";
 
     public BackgroundActionsModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
     }
 
+    @NonNull
     @Override
     public String getName() {
-        return "RNBackgroundActions";
+        return TAG;
     }
 
     @SuppressWarnings("unused")
     @ReactMethod
-    public void start(final Promise promise) {
+    public void start(@NonNull final Promise promise) {
+        promise.resolve(null);
     }
 
     @SuppressWarnings("unused")
     @ReactMethod
-    public void stop(final Promise promise) {
+    public void stop(@NonNull final Promise promise) {
+        promise.resolve(null);
     }
 }
