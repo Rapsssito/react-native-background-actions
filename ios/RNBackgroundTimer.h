0c54b6cc2a7ae277c59a17dfb9e6a915dd1d80e7
Rapsssito <contact@rodrigomartin.dev>
2020-01-14 20:54:03 +0100

    iOS implementation

--- ios/RNBackgroundTimer.h
+++ ios/RNBackgroundTimer.h
@@ -1,14 +1,5 @@
-//
-//  RNBackgroundTimer.h
-//  react-native-background-timer
-//
-//  Created by IjzerenHein on 06-09-2016.
-//  Copyright (c) ATO Gear. All rights reserved.
-//
-
 #import <React/RCTBridgeModule.h>
-#import "RCTEventEmitter.h"
 
-@interface RNBackgroundTimer : RCTEventEmitter <RCTBridgeModule>
+@interface RNBackgroundTimer : NSObject <RCTBridgeModule>
 
 @end
