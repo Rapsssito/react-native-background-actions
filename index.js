1668b0989f184b62d99d7127baee91e340f3aae9
Rapsssito <contact@rodrigomartin.dev>
2020-01-16 16:46:26 +0100

    fix: iOS new API (#6)

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class BackgroundTimer {
             RNBackgroundActions.start(finalOptions);
         } else {
             RNBackgroundActions.start(finalOptions);
-            task();
+            task(finalOptions);
         }
     }
 
