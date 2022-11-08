f55b3904b68f84bd5b7a8a0564550563d79ba374
dineshhannurkar <dineshhannurkar20@gmail.com>
2022-11-08 15:15:32 +0530

    In android 12 devices which is targeting to SDK 31 getting crashed, for this I solved by modifying pending intent

--- android/src/main/java/com/asterinet/react/bgactions/RNBackgroundActionsTask.java
+++ android/src/main/java/com/asterinet/react/bgactions/RNBackgroundActionsTask.java
@@ -1,5 +1,6 @@
 package com.asterinet.react.bgactions;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
