32b707596562f15647bc797235b6c4b04a422750
Hoseong-Ryu <hoseong091234@gmail.com>
2026-04-08 08:17:28 +0900

    chore: remove extra whitespace

--- android/src/main/java/com/asterinet/react/bgactions/RNBackgroundActionsTask.java
+++ android/src/main/java/com/asterinet/react/bgactions/RNBackgroundActionsTask.java
@@ -13,7 +13,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
